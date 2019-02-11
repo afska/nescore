ce44bfd882bde6fc83201ab551343966081e3041
Rodrigo Alfonso <rodri042@gmail.com>
2019-02-11 00:43:30 -0300

    Testing Register8Bit

---
+++ src/nes/registers/Register.spec.js
@@ -0,0 +1,14 @@
+import { Register8Bit } from ".";
+const should = require("chai").Should();
+
+describe("Register8Bit", () => {
+  it("allows initial values", () => {
+    new Register8Bit(5).value.should.eql(5);
+  });
+
+  it("handles overflow correctly", () => {
+    const register = new Register8Bit(250);
+    register.value += 7;
+    register.value.should.eql(1);
+  });
+});
