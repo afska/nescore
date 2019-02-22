5c2d661f70ee6c97810730be9774697c60555463
Rodrigo Alfonso <rodri042@gmail.com>
2019-02-22 12:44:37 -0300

    Adding all the check operations

--- src/nes/operations/index.js
+++ src/nes/operations/index.js
@@ -1,6 +1,7 @@
 import logical from "./logical";
+import checks from "./checks";
 import data from "./data";
 import misc from "./misc";
 import _ from "lodash";
 
-export default _.keyBy([...logical, ...data, ...misc], "id");
+export default _.keyBy([...logical, ...checks, ...data, ...misc], "id");
