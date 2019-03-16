43a0a0e8338bd3da1f881fbb3b1ea80dcad5b9b6
Rodrigo Alfonso <rodri042@gmail.com>
2019-03-16 03:15:38 -0300

    Adding an index file for PPU registers

---
+++ src/nes/ppu/registers/index.js
@@ -0,0 +1,7 @@
+import PPUCtrl from "./PPUCtrl";
+import PPUMask from "./PPUMask";
+import PPUStatus from "./PPUStatus";
+import OAMAddr from "./OAMAddr";
+import OAMData from "./OAMData";
+
+export { PPUCtrl, PPUMask, PPUStatus, OAMAddr, OAMData };
