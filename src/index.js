eeeec990e4cedf1ace7528d5f25920934b175a7a
Rodrigo Alfonso <rodri042@gmail.com>
2019-03-22 03:02:08 -0300

    Adding pixijs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { getIndirectAddress } from "./nes/cpu/addressings/indirect";
 import "./gui";
 import { Byte } from "./nes/helpers";
 import _ from "lodash";
+import * as PIXI from "pixi.js";
 
 const hex = (value, length) =>
 	_.padStart(value.toString(16).toUpperCase(), length, "0");
@@ -197,3 +198,46 @@ const DEMO = async () => {
 };
 
 DEMO();
+
+// ---
+
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application();
+const graphics = new PIXI.Graphics();
+app.stage.addChild(graphics);
+
+let a = 1;
+app.ticker.add(function(delta) {
+	graphics.clear();
+
+	// Rectangle
+	graphics.beginFill(0xff0000);
+	graphics.drawRect(50 + ++a, 50, 100, 100);
+	graphics.endFill();
+});
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+// // This creates a texture from a 'bunny.png' image
+// const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+// // Setup the position of the bunny
+// bunny.x = app.renderer.width / 2;
+// bunny.y = app.renderer.height / 2;
+
+// // Rotate around the center
+// bunny.anchor.x = 0.5;
+// bunny.anchor.y = 0.5;
+
+// // Add the bunny to the scene we are building
+// app.stage.addChild(bunny);
+
+// // Listen for frame updates
+// app.ticker.add(() => {
+// 	// each frame we spin the bunny around a bit
+// 	bunny.rotation += 0.01;
+// });
