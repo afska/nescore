fc2dbad37d91f39ceb25df8182b703a80214bcce
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-18 06:25:19 -0300

    Fixing scanlineType so preline part is scanline -1

--- src/nes/ppu/constants/scanlineType.js
+++ src/nes/ppu/constants/scanlineType.js
@@ -1,11 +1,11 @@
 /** Returns the type of `scanLine`. */
 export default (scanLine) => {
-	if (scanLine < 240) {
+	if (scanLine == -1) {
+		return "PRELINE";
+	} else if (scanLine < 240) {
 		return "VISIBLE";
 	} else if (scanLine === 241) {
 		return "VBLANK_START";
-	} else if (scanLine === 261) {
-		return "PRELINE";
 	} else {
 		return "IDLE";
 	}
