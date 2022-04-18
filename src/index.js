02e1ff621df0534282bbd3912687f8df032ec1a9
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-18 20:21:57 -0300

    Adding nes.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./gui/App";
+import "nes.css/css/nes.css";
 import "./gui/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
