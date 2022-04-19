414ee57dddaafb099a0f09d8023db1c6c89c70d0
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-18 21:22:22 -0300

    Adding canvas with tv noise

--- src/gui/App.js
+++ src/gui/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Emulator from "./components/Emulator";
-import "./App.css";
+import TVNoise from "./components/TVNoise";
+import classNames from "classnames";
+import styles from "./App.module.css";
 
 export default class App extends Component {
 	state = { rom: null };
@@ -9,19 +11,29 @@ export default class App extends Component {
 		const { rom } = this.state;
 
 		return (
-			<div className="app">
+			<div className={styles.app}>
 				<h1>NesCore</h1>
+				<br />
 
-				{/* {rom ? (
-					<Emulator
-						rom={rom}
-						onStartPressed={this._onStartPressed}
-						onError={this._onError}
-						ref={(ref) => (this.emulator = ref)}
-					/>
-				) : (
-					<TVNoise />
-				)} */}
+				<div
+					className={classNames(
+						styles.gameContainer,
+						"nes-container",
+						"is-dark",
+						"with-title"
+					)}
+				>
+					{rom ? (
+						<Emulator
+							rom={rom}
+							onStartPressed={this._onStartPressed}
+							onError={this._onError}
+							ref={(ref) => (this.emulator = ref)}
+						/>
+					) : (
+						<TVNoise />
+					)}
+				</div>
 			</div>
 		);
 	}
