3782ab6a79bef8a9c6ff67a9fc6ecda6e3a93623
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-19 22:39:36 -0300

    Testing PPUStatus

--- src/nes/ppu/registers/PPUStatus.spec.js
+++ src/nes/ppu/registers/PPUStatus.spec.js
@@ -5,17 +5,25 @@ const ADDRESS = 0x2002;
 
 describe("CPU/PPU registers interaction", () => {
 	describe("PPUStatus", () => {
-		let ppu, memory;
+		let ppu, memory, register;
 
 		beforeEach(() => {
 			({ ppu, memory } = createTestContextForCPUMemoryMap());
+			register = ppu.registers.ppuStatus;
+		});
+
+		it("is read-only", () => {
+			register.value = 123;
+			memory.readAt(ADDRESS).should.equal(123);
+			memory.writeAt(ADDRESS, 456);
+			memory.readAt(ADDRESS).should.equal(123);
 		});
 
 		it("can read the register", () => {
 			memory.readAt(ADDRESS).should.equal(0);
 
-			ppu.registers.ppuStatus.spriteOverflow = 1;
-			ppu.registers.ppuStatus.sprite0Hit = 1;
+			register.spriteOverflow = 1;
+			register.sprite0Hit = 1;
 
 			memory.readAt(ADDRESS).should.equal(0b01100000);
 		});
