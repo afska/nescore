2db446427e38c8b840ea183abd7024c18e50727b
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-20 23:41:21 -0300

    Sorting cycleType if

--- src/nes/ppu/constants/cycleType.js
+++ src/nes/ppu/constants/cycleType.js
@@ -6,18 +6,18 @@ export default (cycle) => {
 		return "ONE";
 	} else if (cycle > 1 && cycle < 257) {
 		return "VISIBLE";
+	} else if (cycle === 257) {
+		return "COPY_X";
+	} else if (cycle === 258) {
+		return "INCREMENT_Y";
+	} else if (cycle === 259) {
+		return "SPRITES";
+	} else if (cycle > 279 && cycle < 305) {
+		return "COPY_Y";
 	} else if (cycle === 321) {
 		return "FLUSH_TILEDATA";
 	} else if (cycle > 321 && cycle < 337) {
 		return "PREFETCH";
-	} else if (cycle === 259) {
-		return "SPRITES";
-	} else if (cycle === 258) {
-		return "INCREMENT_Y";
-	} else if (cycle === 257) {
-		return "COPY_X";
-	} else if (cycle > 279 && cycle < 305) {
-		return "COPY_Y";
 	} else if (cycle === 340) {
 		return "MAPPER_TICK";
 	} else {
