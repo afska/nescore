9f873701e89eb57ce386ff7be0a598c6a13e6543
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-25 06:46:22 -0300

    Removing nested ifs

--- src/nes/ppu/pipeline/_2_visibleLines.js
+++ src/nes/ppu/pipeline/_2_visibleLines.js
@@ -4,29 +4,32 @@ const NAMETABLE_ID = 0;
 
 /** Runs for each visible scanline. Renders the image. */
 export default (context) => {
-	renderBackground(context);
+	const { ppu } = context;
+
+	if (
+		ppu.cycle < constants.SCREEN_WIDTH &&
+		ppu.cycle % constants.PPU_RENDER_FREQUENCY === 0
+	)
+		renderBackground(context);
 };
 
 /** Renders the background. */
 const renderBackground = ({ ppu }) => {
-	if (ppu.cycle < constants.SCREEN_WIDTH) {
-		if (ppu.cycle % constants.PPU_RENDER_FREQUENCY === 0) {
-			const y = ppu.scanline;
+	const y = ppu.scanline;
 
-			for (let i = 0; i < constants.PPU_RENDER_FREQUENCY; i++) {
-				let x = ppu.cycle + i;
+	for (let i = 0; i < constants.PPU_RENDER_FREQUENCY; i++) {
+		let x = ppu.cycle + i;
 
-				const paletteId = ppu.attributeTable.getPaletteIdOf(NAMETABLE_ID, x, y);
+		const tileId = ppu.nameTable.getTileIdOf(NAMETABLE_ID, x, y);
+		const paletteId = ppu.attributeTable.getPaletteIdOf(NAMETABLE_ID, x, y);
+		const paletteIndex = ppu.patternTable.getPaletteIndexOf(
+			ppu.registers.ppuCtrl.patternTableAddressIdForBackground,
+			tileId,
+			x % constants.TILE_SIZE,
+			y % constants.TILE_SIZE
+		);
+		const color = ppu.framePalette.getColorOf(paletteId, paletteIndex);
 
-				const paletteIndex = ppu.patternTable.getPaletteIndexOf(
-					ppu.registers.ppuCtrl.patternTableAddressIdForBackground,
-					ppu.nameTable.getTileIdOf(NAMETABLE_ID, x, y),
-					x % constants.TILE_SIZE,
-					y % constants.TILE_SIZE
-				);
-
-				ppu.plot(x, y, ppu.framePalette.getColorOf(paletteId, paletteIndex));
-			}
-		}
+		ppu.plot(x, y, color);
 	}
 };
