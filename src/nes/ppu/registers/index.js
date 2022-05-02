5221d67d85013ab6b812706cc21962a35df8ab97
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-02 04:35:47 -0300

    Adding PPU registers memory locations

--- src/nes/ppu/registers/index.js
+++ src/nes/ppu/registers/index.js
@@ -12,15 +12,15 @@ import { WithCompositeMemory } from "../../memory";
 /** A collection of all the CPU-mapped PPU registers. */
 class PPURegisterSegment {
 	constructor(context) {
-		this.ppuCtrl = new PPUCtrl().loadContext(context);
-		this.ppuMask = new PPUMask().loadContext(context);
-		this.ppuStatus = new PPUStatus().loadContext(context);
-		this.oamAddr = new OAMAddr().loadContext(context);
-		this.oamData = new OAMData().loadContext(context);
-		this.ppuScroll = new PPUScroll().loadContext(context);
-		this.ppuAddr = new PPUAddr().loadContext(context);
-		this.ppuData = new PPUData().loadContext(context);
-		this.oamDma = new OAMDMA().loadContext(context);
+		this.ppuCtrl = new PPUCtrl().loadContext(context); //     $2000
+		this.ppuMask = new PPUMask().loadContext(context); //     $2001
+		this.ppuStatus = new PPUStatus().loadContext(context); // $2002
+		this.oamAddr = new OAMAddr().loadContext(context); //     $2003
+		this.oamData = new OAMData().loadContext(context); //     $2004
+		this.ppuScroll = new PPUScroll().loadContext(context); // $2005
+		this.ppuAddr = new PPUAddr().loadContext(context); //     $2006
+		this.ppuData = new PPUData().loadContext(context); //     $2007
+		this.oamDma = new OAMDMA().loadContext(context); //       $4014
 	}
 
 	/** Creates a memory segment with the first 8 registers ($2000-$2007). */
