e544b4bdc1c8fa7efe9baffbe1f3ce44b9cace55
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-05 20:23:59 -0300

    Adding test benchmark

--- src/gui/App.js
+++ src/gui/App.js
@@ -4,6 +4,7 @@ import TVNoise from "./components/TVNoise";
 import classNames from "classnames";
 import styles from "./App.module.css";
 import _ from "lodash";
+import NES from "../nes";
 
 export default class App extends Component {
 	state = { rom: null };
@@ -55,9 +56,43 @@ export default class App extends Component {
 	}
 
 	_loadRom(rom) {
-		this.setState({ rom }, () => {
-			this.emulator.start();
-		});
+		let frames = 0;
+		let fpsMarks = [];
+		let runTime = 0;
+		let secondTime = 0;
+
+		const bytes = new Uint8Array(rom);
+
+		const nes = new NES();
+		nes.load(bytes);
+
+		while (true) {
+			if (window.stopNow) return;
+
+			const t0 = performance.now();
+			nes.frame();
+			const t1 = performance.now();
+
+			const elapsedMs = t1 - t0;
+			frames++;
+			runTime += elapsedMs;
+			secondTime += elapsedMs;
+
+			if (secondTime > 1000) {
+				console.log(frames);
+				fpsMarks.push(frames);
+				secondTime = 0;
+				frames = 0;
+			}
+
+			if (runTime > 5000) {
+				console.log("AVERAGE:", _.mean(fpsMarks));
+				console.log("RELOADING...");
+				nes.load(bytes);
+				runTime = 0;
+				fpsMarks = [];
+			}
+		}
 	}
 
 	_onFileDrop = (e) => {
