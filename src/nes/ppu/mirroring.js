2f23206bf3b4a3348d3894358bc8717c0f7851c6
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-07 06:46:02 -0300

    Implementing one screen mirrorings

--- src/nes/ppu/mirroring.js
+++ src/nes/ppu/mirroring.js
@@ -1,9 +1,6 @@
 import { RewiredMemoryChunk } from "../memory";
 
-// TODO: IMPLEMENT!!
-const ONE_SCREEN_LOWER_BANK = RewiredMemoryChunk.createMapping([]);
-
-const ONE_SCREEN_UPPER_BANK = RewiredMemoryChunk.createMapping([]);
+// Name tables A and B are located in 0x000 and 0x400 but can be remapped.
 
 const HORIZONTAL = RewiredMemoryChunk.createMapping([
 	{ from: 0x400, size: 0x400, to: 0x000 }, // top-right (A mirror)
@@ -14,6 +11,18 @@ const HORIZONTAL = RewiredMemoryChunk.createMapping([
 const VERTICAL = RewiredMemoryChunk.createMapping([
 	{ from: 0x800, size: 0x400, to: 0x000 }, // bottom-left (A mirror)
 	{ from: 0xc00, size: 0x400, to: 0x400 } // bottom-right (B mirror)
+]);
+
+const ONE_SCREEN_LOWER_BANK = RewiredMemoryChunk.createMapping([
+	{ from: 0x400, size: 0x400, to: 0x000 }, // top-right (A mirror)
+	{ from: 0x800, size: 0x400, to: 0x000 }, // bottom-left (A mirror)
+	{ from: 0xc00, size: 0x400, to: 0x000 } // bottom-right (A mirror)
+]);
+
+const ONE_SCREEN_UPPER_BANK = RewiredMemoryChunk.createMapping([
+	{ from: 0x000, size: 0x400, to: 0x400 }, // top-left (A mirror)
+	{ from: 0x800, size: 0x400, to: 0x400 }, // bottom-left (A mirror)
+	{ from: 0xc00, size: 0x400, to: 0x400 } // bottom-right (A mirror)
 ]);
 
 export default {
