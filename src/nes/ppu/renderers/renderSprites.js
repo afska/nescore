4b4ea0d705e252e48a37bac9f538e1f0fb42dbaf
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-08 04:29:48 -0300

    FIX: Drawing sprites the right way, preserving priorities (SMB3 trick)

--- src/nes/ppu/renderers/renderSprites.js
+++ src/nes/ppu/renderers/renderSprites.js
@@ -28,59 +28,76 @@ const evaluateSprites = ({ ppu }) => {
 	return _.orderBy(sprites, ["id"], ["desc"]);
 };
 
-/** Draws a list of `sprites` on screen. */
+/** Draws a list of `sprites` on the current scanline. */
 const drawSprites = (context, sprites) => {
 	const { ppu } = context;
+	const { ppuMask, ppuStatus } = ppu.registers;
+	const finalY = ppu.scanline;
+
+	const colors = [];
+	const priorities = [];
+	const xs = [];
 
 	for (let sprite of sprites) {
-		const insideY = sprite.diffY(ppu.scanline);
+		const insideY = sprite.diffY(finalY);
 
-		for (let insideX = 0; insideX < constants.TILE_LENGTH; insideX++)
-			drawSpritePixel(context, sprite, insideX, insideY);
+		for (let insideX = 0; insideX < constants.TILE_LENGTH; insideX++) {
+			const finalX = sprite.x + insideX;
+			if (!ppuMask.showSpritesInLeftmost8PixelsOfScreen && finalX < 8) continue;
+
+			// color fetch
+			const paletteId = sprite.paletteId;
+			const paletteIndex = getSpritePixelPaletteIndex(
+				context,
+				sprite,
+				insideX,
+				insideY
+			);
+			const isSpritePixelOpaque = paletteIndex !== constants.COLOR_TRANSPARENT;
+			const isBackgroundPixelOpaque =
+				ppu.paletteIndexOf(finalX, finalY) !== constants.COLOR_TRANSPARENT;
+
+			// sprite 0 hit
+			if (
+				sprite.id === 0 &&
+				isSpritePixelOpaque &&
+				isBackgroundPixelOpaque &&
+				ppuMask.showBackground &&
+				ppuMask.showSprites
+			)
+				ppuStatus.sprite0Hit = 1;
+
+			// add to drawing buffer
+			if (isSpritePixelOpaque) {
+				colors[finalX] = ppu.framePalette.getColorOf(paletteId, paletteIndex);
+				priorities[finalX] = sprite.isInFrontOfBackground;
+				xs[finalX] = finalX;
+			}
+		}
+	}
+
+	for (let finalX of xs) {
+		const isInFrontOfBackground = priorities[finalX];
+		const color = colors[finalX];
+
+		// sprite/background priority
+		const isBackgroundPixelOpaque =
+			ppu.paletteIndexOf(finalX, finalY) !== constants.COLOR_TRANSPARENT;
+		const shouldDraw = isInFrontOfBackground || !isBackgroundPixelOpaque;
+
+		// actual drawing
+		if (shouldDraw) ppu.plot(finalX, finalY, color);
 	}
 };
 
-/** Draws the (`insideX`, `insideY`) `sprite`'s pixel. */
-const drawSpritePixel = ({ ppu }, sprite, insideX, insideY) => {
-	const { ppuMask, ppuStatus } = ppu.registers;
-
-	const finalX = sprite.x + insideX;
-	const finalY = ppu.scanline;
+/** Returns the (`insideX`, `insideY`) `sprite`'s pixel palette index. */
+const getSpritePixelPaletteIndex = ({ ppu }, sprite, insideX, insideY) => {
 	const tileInsideY = insideY % constants.TILE_LENGTH;
 
-	if (!ppuMask.showSpritesInLeftmost8PixelsOfScreen && finalX < 8) return;
-
-	// color fetch
-	const paletteId = sprite.paletteId;
-	const paletteIndex = ppu.patternTable.getPaletteIndexOf(
+	return ppu.patternTable.getPaletteIndexOf(
 		sprite.patternTableId,
 		sprite.tileIdFor(insideY),
 		sprite.flipX ? constants.TILE_LENGTH - 1 - insideX : insideX,
 		sprite.flipY ? constants.TILE_LENGTH - 1 - tileInsideY : tileInsideY
 	);
-	const isSpritePixelTransparent = paletteIndex === constants.COLOR_TRANSPARENT;
-	const isBackgroundPixelTransparent =
-		ppu.paletteIndexOf(finalX, finalY) === constants.COLOR_TRANSPARENT;
-
-	// sprite 0 hit
-	if (
-		sprite.id === 0 &&
-		!isSpritePixelTransparent &&
-		!isBackgroundPixelTransparent &&
-		ppuMask.showBackground &&
-		ppuMask.showSprites &&
-		(finalX >= 8 || ppuMask.showBackgroundInLeftmost8PixelsOfScreen)
-	)
-		ppuStatus.sprite0Hit = 1;
-
-	// sprite/background priority
-	const shouldDraw =
-		!isSpritePixelTransparent &&
-		(sprite.isInFrontOfBackground || isBackgroundPixelTransparent);
-
-	// actual drawing
-	if (shouldDraw) {
-		const color = ppu.framePalette.getColorOf(paletteId, paletteIndex);
-		ppu.plot(finalX, finalY, color);
-	}
 };
