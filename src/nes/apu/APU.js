f53918e9ef650d4dcf6c0ec6c59bcfbddab3eabc
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-17 10:01:39 -0300

    Testing noise

--- src/nes/apu/APU.js
+++ src/nes/apu/APU.js
@@ -9,11 +9,15 @@ export default class APU {
 	/** When a context is loaded. */
 	onLoad(context) {
 		this._reset();
+		this.cycle = 0;
 	}
 
 	/** Executes the next operation (1 cycle). */
 	step(onAudioSample) {
 		// TODO: IMPLEMENT
+
+		this.cycle++;
+		onAudioSample((Math.random() * 2 - 1) * 0.5); // noise
 	}
 
 	_reset() {}
