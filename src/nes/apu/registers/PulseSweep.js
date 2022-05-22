496efbd9ba2e1ac920ea20220fd6c3c991ce2151
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-22 07:55:17 -0300

    Adding pulse sweep parameters

--- src/nes/apu/registers/PulseSweep.js
+++ src/nes/apu/registers/PulseSweep.js
@@ -1,3 +1,12 @@
 import { WriteOnlyInMemoryRegister } from "../../registers";
 
-export default class PulseSweep extends WriteOnlyInMemoryRegister {}
+export default class PulseSweep extends WriteOnlyInMemoryRegister {
+	constructor() {
+		super();
+
+		this.addReadOnlyField("shiftCount", 0, 3)
+			.addReadOnlyField("negateFlag", 3)
+			.addReadOnlyField("dividerPeriodMinusOne", 4, 3)
+			.addReadOnlyField("enabledFlag", 7);
+	}
+}
