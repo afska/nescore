1a7af9937434fc195c1a1eb7790930b47403d714
Rodrigo Alfonso <rodri042@gmail.com>
2022-05-25 23:18:21 -0300

    Adding a minimum frequency change to oscillators to avoid glitches

--- src/nes/apu/synthesis/PulseOscillator.js
+++ src/nes/apu/synthesis/PulseOscillator.js
@@ -6,7 +6,8 @@ export default class PulseOscillator {
 		this.amplitude = 1;
 		this.harmonics = config.PULSE_CHANNEL_HARMONICS;
 		this.dutyCycle = 0;
-		this.frequency = 0;
+
+		this._frequency = 0;
 	}
 
 	/** Generates a new sample. */
@@ -15,13 +16,25 @@ export default class PulseOscillator {
 		let y2 = 0;
 
 		const pi2 = Math.PI * 2;
+		const { amplitude, harmonics, frequency, dutyCycle } = this;
 
-		for (let n = 1; n <= this.harmonics; n++) {
-			y1 += _approxsin(pi2 * this.frequency * time * n) / n;
-			y2 += _approxsin(pi2 * (this.frequency * time - this.dutyCycle) * n) / n;
+		for (let n = 1; n <= harmonics; n++) {
+			y1 += _approxsin(pi2 * frequency * time * n) / n;
+			y2 += _approxsin(pi2 * (frequency * time - dutyCycle) * n) / n;
 		}
 
-		return (y1 - y2) * config.PULSE_CHANNEL_VOLUME * this.amplitude;
+		return (y1 - y2) * config.PULSE_CHANNEL_VOLUME * amplitude;
+	}
+
+	/** Returns the frequency. */
+	get frequency() {
+		return this._frequency;
+	}
+
+	/** Sets the frequency, only if `value` is above a minimum value. */
+	set frequency(value) {
+		if (Math.abs(this._frequency - value) > config.MIN_FREQUENCY_CHANGE)
+			this._frequency = value;
 	}
 }
 
--- src/nes/apu/synthesis/TriangleOscillator.js
+++ src/nes/apu/synthesis/TriangleOscillator.js
@@ -4,9 +4,11 @@ import config from "../../config";
 export default class TriangleOscillator {
 	constructor() {
 		this.amplitude = 1;
-		this.frequency = 0;
+
 		this.lut = [];
 		this._generateLookUpTable();
+
+		this._frequency = 0;
 	}
 
 	/** Generates a new sample. */
@@ -14,13 +16,14 @@ export default class TriangleOscillator {
 		let y = 0;
 
 		const pi2 = Math.PI * 2;
+		const { frequency, amplitude, lut } = this;
 
 		for (let i = 1; i <= config.TRIANGLE_CHANNEL_HARMONICS; i++) {
 			const n = 2 * i + 1;
-			y += this.lut[i] * _approxsin(pi2 * this.frequency * time * n);
+			y += lut[i] * _approxsin(pi2 * frequency * time * n);
 		}
 
-		return y * config.TRIANGLE_CHANNEL_VOLUME * this.amplitude;
+		return y * config.TRIANGLE_CHANNEL_VOLUME * amplitude;
 	}
 
 	_generateLookUpTable() {
@@ -28,6 +31,17 @@ export default class TriangleOscillator {
 			const n = 2 * i + 1;
 			this.lut[i] = Math.pow(-1, i) * Math.pow(n, -2);
 		}
+	}
+
+	/** Returns the frequency. */
+	get frequency() {
+		return this._frequency;
+	}
+
+	/** Sets the frequency, only if `value` is above a minimum value. */
+	set frequency(value) {
+		if (Math.abs(this._frequency - value) > config.MIN_FREQUENCY_CHANGE)
+			this._frequency = value;
 	}
 }
 
