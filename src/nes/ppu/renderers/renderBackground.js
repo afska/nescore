afska/nescore

commit 67163936f538545b4af65be12c95ea24062294fb

Cleaning PPU code

src/nes/ppu/renderers/renderBackground.js
@@ -18,7 +18,6 @@ export default function renderBackground({ ppu }) {
 		if (!registers.ppuMask.showBackgroundInLeftmost8PixelsOfScreen && x < 8) {
 			ppu.plot(x, y, transparentColor);
 			ppu.savePaletteIndex(x, y, 0);
-			loopy.onVisibleLine(cycle);
 			continue;
 		}
 

src/nes/ppu/PPU.js
@@ -53,7 +53,8 @@ export default class PPU {
 	}
 
 	/**
-	 * Executes the next step (1 step = 1 PPU cycle). Returns an interrupt or null.
+	 * Executes the next step (1 step = 1 PPU cycle).
+	 * Returns an interrupt or null.
 	 * It calls `onFrame` when it generates a new frame.
 	 */
 	step(onFrame) {

commit fe4cd58cab712cf2d97aa30e1a0f9bfac2709444

Fixing regression introduced in 67163936f538545b4af65be12c95ea24062294fb

src/nes/ppu/renderers/renderBackground.js
@@ -18,6 +18,7 @@ export default function renderBackground({ ppu }) {
 		if (!registers.ppuMask.showBackgroundInLeftmost8PixelsOfScreen && x < 8) {
 			ppu.plot(x, y, transparentColor);
 			ppu.savePaletteIndex(x, y, 0);
+			loopy.onVisibleLine(cycle);
 			continue;
 		}
 