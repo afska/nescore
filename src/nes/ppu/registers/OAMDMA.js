66c6fbdd4c68d43b038f3c340292d41a6ec9d906
Rodrigo Alfonso <rodri042@gmail.com>
2023-04-04 07:56:08 -0300

    Removing unused variable

--- src/nes/ppu/registers/OAMDMA.js
+++ src/nes/ppu/registers/OAMDMA.js
@@ -11,7 +11,7 @@ import { Byte } from "../../helpers";
 export default class OAMDMA extends WriteOnlyInMemoryRegister {
 	/** Writes 256 bytes from page $`byte` to PPU's internal OAM. */
 	writeAt(__, byte) {
-		const { cpu, ppu, memoryBus } = this.context;
+		const { cpu, ppu } = this.context;
 
 		for (let i = 0; i < 256; i++) {
 			const address = Byte.to16Bit(byte, i);
