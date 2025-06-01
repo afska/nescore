3be3226d72a452fd669e169ddcc243eb13a15ded
Rodrigo Alfonso <rodri042@gmail.com>
2025-06-01 08:18:19 -0300

    Adding fade in to emulator frontend

--- src/gui/emulator/Speaker.js
+++ src/gui/emulator/Speaker.js
@@ -4,6 +4,7 @@ import constants from "../../nes/constants";
 const WORKLET_NAME = "player-worklet";
 const WEBAUDIO_BUFFER_SIZE = 1024;
 const CHANNELS = 1;
+const FADE_DURATION = 0.05; // seconds
 
 export default class Speaker {
 	async start() {
@@ -11,26 +12,39 @@ export default class Speaker {
 		if (!window.AudioContext) return;
 
 		this.bufferSize = 0;
-
 		this._audioCtx = new window.AudioContext({
 			sampleRate: constants.APU_SAMPLE_RATE
 		});
 
 		await this._audioCtx.audioWorklet.addModule(audioWorklet);
-		if (this._audioCtx == null) {
+		if (!this._audioCtx) {
 			this.stop();
 			return;
 		}
 
+		// create gain node at 0
+		this.gainNode = this._audioCtx.createGain();
+		this.gainNode.gain.setValueAtTime(0, this._audioCtx.currentTime);
+
 		this.playerWorklet = new AudioWorkletNode(this._audioCtx, WORKLET_NAME, {
 			outputChannelCount: [CHANNELS],
-			processorOptions: {
-				bufferSize: WEBAUDIO_BUFFER_SIZE
-			}
+			processorOptions: { bufferSize: WEBAUDIO_BUFFER_SIZE }
 		});
-		this.playerWorklet.connect(this._audioCtx.destination);
+
+		// worklet -> gain -> destination
+		this.playerWorklet.connect(this.gainNode);
+		this.gainNode.connect(this._audioCtx.destination);
+
+		this._fadedIn = false;
 		this.playerWorklet.port.onmessage = (event) => {
 			this.bufferSize = event.data;
+			if (!this._fadedIn && this.bufferSize >= WEBAUDIO_BUFFER_SIZE) {
+				const now = this._audioCtx.currentTime;
+				this.gainNode.gain.cancelScheduledValues(now);
+				this.gainNode.gain.setValueAtTime(0, now);
+				this.gainNode.gain.linearRampToValueAtTime(1, now + FADE_DURATION);
+				this._fadedIn = true;
+			}
 		};
 	}
 
@@ -54,10 +68,14 @@ export default class Speaker {
 			this.playerWorklet.disconnect();
 			this.playerWorklet = null;
 		}
-
+		if (this.gainNode) {
+			this.gainNode.disconnect();
+			this.gainNode = null;
+		}
 		if (this._audioCtx) {
 			this._audioCtx.close().catch(console.error);
 			this._audioCtx = null;
 		}
+		this._fadedIn = false;
 	}
 }
