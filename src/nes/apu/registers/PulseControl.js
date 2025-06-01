e50d64fd0ced2fead72540952d2e0d784508cc8b
Rodrigo Alfonso <rodri042@gmail.com>
2025-06-01 07:51:34 -0300

    FIX: APU Pulse 50% duty cycle

--- src/nes/apu/registers/PulseControl.js
+++ src/nes/apu/registers/PulseControl.js
@@ -1,6 +1,6 @@
 import { WriteOnlyInMemoryRegister } from "../../registers";
 
-const DUTY_CYCLES = [0.125, 0.25, 0.25, 0.75];
+const DUTY_CYCLES = [0.125, 0.25, 0.5, 0.75];
 
 /** Sets options for a Pulse channel. */
 export default class PulseControl extends WriteOnlyInMemoryRegister {
@@ -13,7 +13,7 @@ export default class PulseControl extends WriteOnlyInMemoryRegister {
 			.addReadOnlyField("dutyCycleId", 6, 2);
 	}
 
-	/** Returns the duty cycle (0.125, 0.25, 0.25, or 0.75). */
+	/** Returns the duty cycle (0.125, 0.25, 0.5, or 0.75). */
 	get dutyCycle() {
 		return DUTY_CYCLES[this.dutyCycleId];
 	}
