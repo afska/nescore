cfb3277ea6382acc8c2e7c3a59f2b0d6fed8df52
Rodrigo Alfonso <rodri042@gmail.com>
2025-08-05 00:32:58 -0300

    Fixing off-by-one in dpcm wrap

--- src/nes/apu/channels/DMCChannel.js
+++ src/nes/apu/channels/DMCChannel.js
@@ -130,7 +130,7 @@ export default class DMCChannel {
 			let address = this.sampleAddress + this.cursorByte;
 			if (address > 0xffff) {
 				// (if it exceeds $FFFF, it is wrapped around to $8000)
-				address = 0x8000 + (address % 0xffff);
+				address = 0x8000 + (address % 0x10000);
 			}
 			this.buffer = this.context.cpu.memory.readAt(address);
 		}
